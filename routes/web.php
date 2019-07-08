d9bb908a6d478dcfca2a82bf36a13ff62b94b66c
大宮　葵 <aoi0038@gmail.com>
2019-07-08 06:12:32 +0000

    PHP課題09

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+//課題
+
+//3
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、
+//AAAControllerのbbbというAction に渡す
+//Routingの設定」を書いてみてください。
+
+Route::group(['prefix','xxxxx'], function(){
+  Route::get('xxxxx','AAA\NewsController@bbb');
+});
+
+//4
+//admin/profile/create にアクセスしたら ProfileController の 
+//add Action に、admin/profile/edit にアクセスしたら 
+//ProfileController の edit Action に割り当てるように設定してください。
+
+Route::group(['prefix','admin'], function(){
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
