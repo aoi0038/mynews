c1152858fa3015860bd39539ea30705a88254296
大宮　葵 <aoi0038@gmail.com>
2019-07-08 08:53:32 +0000

    profileのbladeファイルの作成

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MyNews</title>
+    <title>profile編集</title>
   </head>
   <body>
-    <h1>Myニュース作成画面</h1>
+    <h1>profile編集</h1>
   </body>
 </html>
